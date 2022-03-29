a46c0fa430cdd083fbdf954f50f25ed41cdaca11
spykelion <ndilionel6@gmail.com>
2022-03-29 10:18:40 +0100

    also add context

---
+++ Data/NotepadContext.cs
@@ -0,0 +1,22 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notepad.Models;
+
+namespace Notepad.Data
+{
+    public class NotepadContext : DbContext
+    {
+        public NotepadContext (DbContextOptions<NotepadContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Notepad.Models.User> User { get; set; }
+
+        public DbSet<Notepad.Models.Note> Note { get; set; }
+    }
+}
